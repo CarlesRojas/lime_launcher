225cebabfc511ed1dd3afa1a9e15c16c78016975
CarlesRojas <carles.rojas@mundimoto.com>
2023-02-26 16:45:32 +0100

    create app list menu

--- app/src/main/java/app/pinya/lime/domain/model/menus/AppListMenu.kt
+++ app/src/main/java/app/pinya/lime/domain/model/menus/AppListMenu.kt
@@ -5,4 +5,5 @@ import androidx.constraintlayout.widget.ConstraintLayout
 data class AppListMenu(
     val isTime: Boolean,
     val container: ConstraintLayout,
+    val onChangeAppCallback: (useDefault: Boolean, useNone: Boolean, newAppPackage: String) -> Unit
 )
